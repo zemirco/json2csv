872b9169e25a2cdcf0224e9ba04b11cf072f7d92
Mirco <mirco.zeiss@gmail.com>
2012-10-24 22:14:47 +0200

    using the new api for testing

--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ var _in = require('./fixtures/in'),
     _out_selected = '',
     _out_reversed = '';
 
-describe('json2csv.parse', function() {
+describe('json2csv', function() {
   
   before(function(done) {
     async.parallel([
@@ -51,46 +51,46 @@ describe('json2csv.parse', function() {
   });
   
   it('should parse json to csv', function(done) {
-    _out.should.equal(json2csv.parse({
-      data: _in,
-      fields: ['carModel', 'price', 'color']
-    }));
-    done();
+    json2csv({data: _in, fields: ['carModel', 'price', 'color']}, function(csv) {
+      csv.should.equal(_out);
+      done();
+    })
   });
   
   it('should throw an error if field is not a key in the json data', function(done) {
-    (function() {
-      json2csv.parse({data: _in, fields: ['carModel', 'location', 'color']});
-    }).should.throwError();
+    (function() {json2csv({
+      data: _in, 
+      fields: ['carModel', 'location', 'color']});
+    }).should.throwError('Cannot find location as a json key');
     done();
   });
   
   it('should output only selected fields', function(done) {
-    _out_selected.should.equal(json2csv.parse({
-      data: _in,
-      fields: ['carModel', 'price']
-    }));
-    done();
+    json2csv({data: _in, fields: ['carModel', 'price']}, function(csv) {
+      csv.should.equal(_out_selected);
+      done();
+    })
   });
         
   it('should output reversed order', function(done) {
-    _out_reversed.should.equal(json2csv.parse({
-      data: _in,
-      fields: ['price', 'carModel']
-    }));
-    done();
+    json2csv({data: _in, fields: ['price', 'carModel']}, function(csv) {
+      csv.should.equal(_out_reversed);
+      done();
+    })
   });
   
   it('should output a string', function(done) {
-    json2csv.parse({data: _in, fields: ['carModel', 'price', 'color']}).should.be.a('string');
-    done();
+    json2csv({data: _in, fields: ['carModel', 'price', 'color']}, function(csv) {
+      csv.should.be.a('string');
+      done();
+    })
   });
     
   it('should escape quotes with double quotes', function(done) {
-    (json2csv.parse({data: _in_quotes, fields: ['a string']}))
-      .should
-      .equal(_out_quotes);
-    done();
+    json2csv({data: _in_quotes, fields: ['a string']}, function(csv) {
+      csv.should.equal(_out_quotes);
+      done();
+    })
   });
     
 });
