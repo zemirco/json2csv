2dd7d0ec4a48a85560441d09716599c20befd58e
kingcody <me@codymize.com>
2014-08-11 04:56:10 -0400

    adding initial Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt) {
+  
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Load grunt tasks automatically, when needed
+  require('jit-grunt')(grunt, {});
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    
+    
+  });
+
+  // Define the grunt tasks
+  grunt.registerTask('default', []);
+
+};
