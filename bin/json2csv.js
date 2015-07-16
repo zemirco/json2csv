516f100df02627696896c30db66d64668e0f688d
Ilya Radchenko <ilya@burstcreations.com>
2015-07-15 20:40:09 -0400

    eslint bin file

--- bin/json2csv.js
+++ bin/json2csv.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var os = require('os');
 var path = require('path');
+var debug = require('debug')('json2csv:cli');
 var program = require('commander');
 var Table = require('cli-table');
 var json2csv = require('../lib/json2csv');
@@ -20,12 +21,19 @@ program
   .option('-p, --pretty', 'Use only when printing to console. Logs output in pretty tables.')
   .parse(process.argv);
 
-if(!program.fields && !program.fieldList) throw new Error('Please specify fields with -f or a list of fields with -l. See json2csv --help');
+if(!program.fields && !program.fieldList) {
+  throw new Error('Please specify fields with -f or a list of fields with -l. See json2csv --help');
+}
 
-var getFields = function(callback) {
+function getFields(callback) {
+  var fields;
+
   if (program.fieldList) {
-    fs.readFile(program.fieldList, 'utf8', function(err, data) {
-      if (err) callback(err);
+    fs.readFile(program.fieldList, 'utf8', function (err, data) {
+      if (err) {
+        return callback(err);
+      }
+
       data.replace(/\r\n|\n\r|\r|\n/g, os.EOL);
       fields = data.split(os.EOL);
       callback(null, fields);
@@ -34,9 +42,9 @@ var getFields = function(callback) {
     fields = program.fields.split(',');
     callback(null, fields);
   }
-};
+}
 
-var getInput = function(callback) {
+function getInput(callback) {
   var input;
 
   if (program.input) {
@@ -48,61 +56,83 @@ var getInput = function(callback) {
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
 
-  process.stdin.on('data', function(chunk) {
+  process.stdin.on('data', function (chunk) {
     input += chunk;
   });
-  process.stdin.on('error', function(err) {
-    console.error('Could not read from stdin', err);
+  process.stdin.on('error', function (err) {
+    debug('Could not read from stdin', err);
   });
-  process.stdin.on('end', function() {
+  process.stdin.on('end', function () {
     callback(null, JSON.parse(input));
   });
-};
+}
 
-var logPretty = function(csv, callback) {
+function logPretty(csv, callback) {
   var lines = csv.split(os.EOL);
   var table = new Table({
     head: lines[0].split(','),
-    colWidths: lines[0].split('","').map(function(elem) {
+    colWidths: lines[0].split('","').map(function (elem) {
       return elem.length * 2;
     })
   });
 
   for (var i = 1; i < lines.length; i++) {
     table.push(lines[i].split('","'));
+
     if (i === lines.length - 1) {
       callback(table.toString());
     }
   }
-};
+}
 
-getFields(function(err, fields) {
-  if (err) throw new Error('Cannot read fields from file ' + program.fieldList);
+getFields(function (err, fields) {
+  if (err) {
+    throw new Error('Cannot read fields from file ' + program.fieldList);
+  }
 
-  getInput(function(err, input) {
-    var opts = {data: input, fields: fields};
+  getInput(function (inputError, input) {
+    if (inputError) {
+      throw new Error('Couldn\'t get the input: ' + inputError);
+    }
+
+    var opts = {
+      data: input,
+      fields: fields
+    };
 
     opts.hasCSVColumnTitle = program.header;
 
-    if (program.delimiter) opts.del = program.delimiter;
-    if (program.eol) opts.eol = program.eol;
+    if (program.delimiter) {
+      opts.del = program.delimiter;
+    }
 
-    json2csv(opts, function(err, csv) {
-      if (err) console.log(err);
+    if (program.eol) {
+      opts.eol = program.eol;
+    }
+
+    json2csv(opts, function (csvError, csv) {
+      if (csvError) {
+        debug(csvError);
+      }
 
       if (program.output) {
-        fs.writeFile(program.output, csv, function(err) {
-          if (err) throw new Error('Cannot save to ' + program.output);
-          console.log(program.input + ' successfully converted to ' + program.output);
+        fs.writeFile(program.output, csv, function (writeError) {
+          if (writeError) {
+            throw new Error('Cannot save to ' + program.output + ': ' + writeError);
+          }
+
+          debug(program.input + ' successfully converted to ' + program.output);
         });
       } else {
+        /*eslint-disable no-console */
         if (program.pretty) {
-          logPretty(csv, function(res) {
+          logPretty(csv, function (res) {
             console.log(res);
           });
         } else {
           console.log(csv);
         }
+        /*eslint-enable no-console */
       }
     });
   });
