de296d3ee23a549b304632ca7e537736611b5f89
Juanjo Diaz <juanjo.diazmo@gmail.com>
2019-04-03 03:10:08 +0300

    feat: Add support for JSON and Streams on the parseAsync method (#377)

--- test/JSON2CSVAsyncParser.js
+++ test/JSON2CSVAsyncParser.js
@@ -4,13 +4,42 @@ const { Readable, Transform, Writable } = require('stream');
 const { AsyncParser, parseAsync } = require('../lib/json2csv');
 
 module.exports = (testRunner, jsonFixtures, csvFixtures, inMemoryJsonFixtures) => {
-  testRunner.add('should parse json to csv, infer the fields automatically and not modify the opts passed using parseAsync method', (t) => {
+  testRunner.add('should parse in-memory json array to csv, infer the fields automatically and not modify the opts passed using parseAsync method', (t) => {
     const opts = {
       fields: ['carModel', 'price', 'color', 'transmission']
     };
-    const transformOpts = { objectMode: true };
 
-    parseAsync(inMemoryJsonFixtures.default, opts, transformOpts)
+    parseAsync(inMemoryJsonFixtures.default, opts)
+      .then((csv) => {
+        t.ok(typeof csv === 'string');
+        t.equal(csv, csvFixtures.default);
+        t.deepEqual(opts, { fields: ['carModel', 'price', 'color', 'transmission'] });
+      })
+      .catch(err => t.notOk(true, err.message))
+      .then(() => t.end());
+  });
+  
+  testRunner.add('should parse in-memory json object to csv, infer the fields automatically and not modify the opts passed using parseAsync method', (t) => {
+    const opts = {
+      fields: ['carModel', 'price', 'color', 'transmission']
+    };
+
+    parseAsync({ "carModel": "Audi",      "price": 0,  "color": "blue" }, opts)
+      .then((csv) => {
+        t.ok(typeof csv === 'string');
+        t.equal(csv, '"carModel","price","color","transmission"\n"Audi",0,"blue",');
+        t.deepEqual(opts, { fields: ['carModel', 'price', 'color', 'transmission'] });
+      })
+      .catch(err => t.notOk(true, err.message))
+      .then(() => t.end());
+  });
+
+  testRunner.add('should parse streaming json to csv, infer the fields automatically and not modify the opts passed using parseAsync method', (t) => {
+    const opts = {
+      fields: ['carModel', 'price', 'color', 'transmission']
+    };
+
+    parseAsync(jsonFixtures.default(), opts)
       .then((csv) => {
         t.ok(typeof csv === 'string');
         t.equal(csv, csvFixtures.default);
