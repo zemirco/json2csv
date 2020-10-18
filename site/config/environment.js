7ab1fb597564dabdbd021eef0017cf20835169d6
Ilya Radchenko <knownasilya@gmail.com>
2020-10-18 17:07:54 -0400

    fix: rooturl and dep clenaup

--- site/config/environment.js
+++ site/config/environment.js
@@ -45,6 +45,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/json2csv/';
   }
 
   return ENV;
